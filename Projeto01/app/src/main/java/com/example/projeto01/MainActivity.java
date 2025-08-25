d70faaee4469c641ff4b0750d59b4fe631361201
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-25 20:53:07 -0300

    Primeiro projeto

--- Projeto01/app/src/main/java/com/example/projeto01/MainActivity.java
+++ Projeto01/app/src/main/java/com/example/projeto01/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.projeto01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button meuBotao;
+    private EditText meuTexto;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        meuBotao = findViewById(R.id.Botao);
+        meuTexto = findViewById(R.id.Texto);
+        resultado = findViewById(R.id.Resultado);
+
+        meuBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoDigitado = meuTexto.getText().toString().trim();
+                resultado.setText(textoDigitado);
+            }
         });
     }
 }
